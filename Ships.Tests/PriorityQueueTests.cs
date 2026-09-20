20b622493482313fdd97fbec48bf16200556d4f4
Joe-vis <joel.visakorpi@gmail.com>
2026-09-21 00:00:12 +0300

    priority queue test pt.2

--- Ships.Tests/PriorityQueueTests.cs
+++ Ships.Tests/PriorityQueueTests.cs
@@ -26,6 +26,52 @@ public class PriorityQueueTests
     }
 
     [Fact]
+    public void Enqueue_MultipleSameNumbers()
+    {
+        var queue = new GC.PriorityQueue<int>();
+        queue.Enqueue(1);
+        queue.Enqueue(1);
+        queue.Enqueue(1);
+        queue.Enqueue(1);
+        queue.Enqueue(1);
+
+        Assert.Equal(5, queue.Count);
+        Assert.True(queue.IsConsistant());
+    }
+
+    /// <summary>
+    /// You can insert a null value
+    /// which can't be compared so they breaks the queue
+    /// </summary>
+    [Fact]
+    public void Enqueue_null()
+    {
+        var queue = new GC.PriorityQueue<string?>();
+        queue.Enqueue("a");
+        queue.Enqueue(null);
+
+        Assert.Equal(1, queue.Count);
+        Assert.True(queue.IsConsistant());
+    }
+
+    [Fact]
+    public void Enqueue_String()
+    {
+        var queue = new GC.PriorityQueue<string>();
+        queue.Enqueue("b");
+        queue.Enqueue("a");
+        queue.Enqueue("c");
+        queue.Enqueue("adawd");
+        queue.Enqueue("adawd");
+
+
+        Assert.Equal(5, queue.Count);
+        Assert.True(queue.IsConsistant());
+        Assert.Equal("a", queue.Peek());
+
+    }
+
+    [Fact]
     public void Enqueue_NegativeNumbers()
     {
         var queue = new GC.PriorityQueue<int>();
@@ -34,6 +80,8 @@ public class PriorityQueueTests
 
         Assert.Equal(2, queue.Count);
         Assert.True(queue.IsConsistant());
+        Assert.Equal(-3, queue.Peek());
+
     }
 
     [Fact]
